e849ac0c114310c79f5d0e4f34aa2ebe2e29750e
kenlove90 <164673905+kenlove90@users.noreply.github.com>
2024-03-25 12:31:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/kenlove90/tun/blob/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/kenlove90/tun/raw/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
